f8126d36fe0ad1c97dcb852181d826760d76c326
brij2804 <brij2804@users.noreply.github.com>
2021-04-14 14:10:30 +0530

    adding hackerrank code

--- DataStructures/Trees/LevelOrderTraversal.java
+++ DataStructures/Trees/LevelOrderTraversal.java
@@ -13,15 +13,16 @@ public static void levelOrder(Node root) {
         q.add(root);
 
         while(!q.isEmpty()){
-        Node tempNode = q.poll();
-        System.out.print(tempNode.data+" ");
-        if(tempNode.left!=null){
-        q.add(tempNode.left);
-        }
-        if(tempNode.right!=null){
-        q.add(tempNode.right);
-        }
+            Node tempNode = q.poll();
+
+            System.out.print(tempNode.data+" ");
+            if(tempNode.left!=null){
+                q.add(tempNode.left);
+            }
+            if(tempNode.right!=null){
+                q.add(tempNode.right);
+            }
         }
 
-        }
+}
 
