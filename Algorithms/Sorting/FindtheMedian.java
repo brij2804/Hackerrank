42a8be962d3db155f8c0b4fb134c1b7e6a770243
brij2804 <brij2804@users.noreply.github.com>
2021-06-08 17:47:51 +0530

    committing algo solutions

---
+++ Algorithms/Sorting/FindtheMedian.java
@@ -0,0 +1,108 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'findMedian' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int findMedian(List<Integer> arr) {
+        // Write your code here
+
+        int pivot= (arr.size()/2);
+        Integer[] newarr=new Integer[arr.size()];
+        newarr=arr.toArray(newarr);
+
+        quickSort(newarr, 0, arr.size() - 1);
+
+        return newarr[pivot];
+
+
+    }
+
+    static void swap(Integer[] arr, int i, int j)
+    {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    static int partition(Integer[] arr, int low, int high)
+    {
+
+        // pivot
+        int pivot = arr[high];
+
+        // Index of smaller element and
+        // indicates the right position
+        // of pivot found so far
+        int i = (low - 1);
+
+        for(int j = low; j <= high - 1; j++)
+        {
+
+            // If current element is smaller
+            // than the pivot
+            if (arr[j] < pivot)
+            {
+
+                // Increment index of
+                // smaller element
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, high);
+        return (i + 1);
+    }
+
+    static void quickSort(Integer[] arr, int low, int high)
+    {
+        if (low < high)
+        {
+
+            // pi is partitioning index, arr[p]
+            // is now at right place
+            int pi = partition(arr, low, high);
+
+            // Separately sort elements before
+            // partition and after partition
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = Result.findMedian(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
--- Algorithms/Sorting/README.md
+++ Algorithms/Sorting/README.md
@@ -8,7 +8,7 @@
 |Counting Sort 1|[Problem](https://www.hackerrank.com/challenges/countingsort1/problem)|java|[Solution](./CountingSort1.java)|
 |Counting Sort 2|[Problem](https://www.hackerrank.com/challenges/countingsort2/problem)|java|[Solution](./CountingSort2.java)|
 |Running Time of Algorithms|[Problem](https://www.hackerrank.com/challenges/runningtime/problem)|java|[Solution](./RunningTimeofAlgorithms.java)|
-
+|Find the Median|[Problem](https://www.hackerrank.com/challenges/find-the-median/problem)|java|[Solution](./FindtheMedian.java)|
 
 
 
